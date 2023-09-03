7f0aa9fe54bdc84e3eca072bdefb69734ed570a5
tiyo <sutiyonodoang@gmail.com>
2023-09-03 23:02:52 +0700

    Create check_directory.sh
    1

---
+++ check_directory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+path="/hdfs/data/data1"
+name_of_directory="data1"
+
+# Check if the directory exists
+if [ -d "$path/$name_of_directory" ]; then
+    echo "There is $name_of_directory Directory Exists!"
+else
+    echo "$name_of_directory Directory Not Exists!"
+    mkdir -p "$path/$name_of_directory"
+    echo "Created $name_of_directory Directory"
+fi
